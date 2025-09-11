efe097c2b4b3da5fee3b9cacb511931c8ce771cd
Aaron Lopez <aaronlopez.fr@gmail.com>
2025-09-10 20:09:24 -0500

    Solucion 5 + Clase publica Validaciones, falta aplicar color y estilo

---
+++ Laboratorio2_DSV_Grupo2/ConjeturaUlam.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Laboratorio2_DSV_Grupo2
+{
+    partial class ConjeturaUlam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.listResultados = new System.Windows.Forms.ListBox();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(375, 43);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(94, 13);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Conjetura de Ulam";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(218, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese un Numero:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(352, 99);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // listResultados
+            // 
+            this.listResultados.FormattingEnabled = true;
+            this.listResultados.Location = new System.Drawing.Point(294, 155);
+            this.listResultados.Name = "listResultados";
+            this.listResultados.Size = new System.Drawing.Size(238, 147);
+            this.listResultados.TabIndex = 3;
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(477, 96);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnEjecutar.TabIndex = 4;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // ConjeturaUlam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.listResultados);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "ConjeturaUlam";
+            this.Text = "ConjeturaUlam";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.ListBox listResultados;
+        private System.Windows.Forms.Button btnEjecutar;
+    }
+}
---
+++ Laboratorio2_DSV_Grupo2/ConjeturaUlam.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio2_DSV_Grupo2
+{
+    public partial class ConjeturaUlam : Form
+    {
+        public ConjeturaUlam()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            String num = txtNumero.Text;
+
+            if (Validaciones.EsNumeroEntero(num))
+            {
+                conjeturaUlam(int.Parse(num));
+            }
+        }
+
+        private void conjeturaUlam (int numero)
+        {
+            listResultados.Items.Clear();
+            while (numero != 1)
+            {
+                if (numero % 2 == 0)
+                {
+                    numero /= 2;
+                }
+                else
+                {
+                    numero = 3 * numero + 1;
+                }
+                listResultados.Items.Add($"{numero}");
+            }
+        }
+    }
+}
